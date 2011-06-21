83dc6d6494c650815dc8ee56db8dc77039559fda
isudana <isudana@gmail.com>
2011-06-21 18:15:00 +0530

    Refactoring ThisUserArticlesComposite. Making dialog resizable

--- bundles/org.eclipse.ecf.salvo.ui/src/org/eclipse/ecf/salvo/ui/internal/dialogs/ThisUserArticlesComposite.java
+++ bundles/org.eclipse.ecf.salvo.ui/src/org/eclipse/ecf/salvo/ui/internal/dialogs/ThisUserArticlesComposite.java
@@ -25,12 +25,7 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -49,24 +44,17 @@ public class ThisUserArticlesComposite extends Composite {
 
 	public static void showArticles(Shell parentShell, INewsgroup newsgroup) {
 
-		Shell shell = new Shell(parentShell);
+		Shell shell = new Shell(parentShell, SWT.SHELL_TRIM);
 		shell.setText("Articles by "
 				+ newsgroup.getServer().getServerConnection().getUser()
 				+ " in " + newsgroup.getNewsgroupName());
 
-		ThisUserArticlesComposite instance = new ThisUserArticlesComposite(
-				shell, newsgroup, SWT.NULL);
+		new ThisUserArticlesComposite(shell, newsgroup, SWT.NULL);
 
 		if (shell.getChildren().length != 0) { // check whether the composite is
 												// already disposed due to
 												// absence of articles
-
-			Point size = instance.getSize();
-			Rectangle shellBounds = shell.computeTrim(0, 0, size.x, size.y);
-			shell.setSize(shellBounds.width, shellBounds.height);
 			shell.setLayout(new FillLayout());
-			shell.layout();
-
 			shell.open();
 		}
 
@@ -84,35 +72,14 @@ public class ThisUserArticlesComposite extends Composite {
 	private void initUIComponents() {
 		try {
 
-			FormLayout thisLayout = new FormLayout();
-			this.setLayout(thisLayout);
-			this.setSize(535, 500);
-
-			{
-				articleBodyComposite = new Composite(this, SWT.BORDER);
-
-				FormData articleBodyCompositeLData = new FormData();
-				articleBodyCompositeLData.left = new FormAttachment(0, 1000, 5);
-				articleBodyCompositeLData.top = new FormAttachment(50, 1000,
-						350);
-				articleBodyCompositeLData.width = 520;
-				articleBodyCompositeLData.height = 100;
-				articleBodyComposite.setLayoutData(articleBodyCompositeLData);
-				articleBodyComposite.setLayout(new GridLayout());
-
-			}
+			this.setLayout(new GridLayout());
 
 			{
 				articleTable = new Table(this, SWT.BORDER);
 				articleTable.setHeaderVisible(true);
 				articleTable.setLinesVisible(true);
-
-				FormData articleTableLData = new FormData();
-				articleTableLData.left = new FormAttachment(0, 1000, 5);
-				articleTableLData.top = new FormAttachment(50, 1000, 0);
-				articleTableLData.width = 503;
-				articleTableLData.height = 320;
-				articleTable.setLayoutData(articleTableLData);
+				articleTable.setLayoutData(new GridData(SWT.FILL, SWT.FILL,
+						true, true));
 
 				subjectColumn = new TableColumn(articleTable, SWT.NONE);
 				subjectColumn.setText("Subject");
@@ -130,9 +97,18 @@ public class ThisUserArticlesComposite extends Composite {
 
 					public void widgetDefaultSelected(SelectionEvent arg0) {
 					}
+
 				});
 
 			}
+
+			{
+				articleBodyComposite = new Composite(this, SWT.BORDER);
+				articleBodyComposite.setLayout(new GridLayout());
+				articleBodyComposite.setLayoutData(new GridData(SWT.FILL,
+						SWT.FILL, true, true));
+			}
+
 			this.layout();
 
 		} catch (Exception e) {
@@ -151,6 +127,7 @@ public class ThisUserArticlesComposite extends Composite {
 		}
 
 		for (IArticle article : articles) {
+
 			TableItem tableItem = new TableItem(articleTable, SWT.NONE);
 			tableItem.setText(0, article.getSubject());
 			tableItem.setText(1, article.getHeaderAttributeValue("Date:"));
@@ -160,7 +137,6 @@ public class ThisUserArticlesComposite extends Composite {
 
 	private void showArticleBody() {
 		IArticle article = articles[articleTable.getSelectionIndex()];
-
 		StringBuffer buffer = new StringBuffer();
 
 		try {
@@ -185,8 +161,6 @@ public class ThisUserArticlesComposite extends Composite {
 			}
 
 			ArticleWidgetBuilder.build(articleBodyComposite, article, handler);
-			articleBodyComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL,
-					true, true));
 			articleBodyComposite.layout(true);
 
 		} catch (Exception e) {
