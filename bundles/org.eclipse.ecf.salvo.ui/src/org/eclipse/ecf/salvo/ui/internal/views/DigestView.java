b97657510f35e9aa2e1556fb63ac25d123560f4e
isudana <isudana@gmail.com>
2011-06-28 23:23:12 +0530

    Initial commit for digest view

---
+++ bundles/org.eclipse.ecf.salvo.ui/src/org/eclipse/ecf/salvo/ui/internal/views/DigestView.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ *  Copyright (c) 2011 University Of Moratuwa
+ *                                                                      
+ * All rights reserved. This program and the accompanying materials     
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at             
+ * http://www.eclipse.org/legal/epl-v10.html                            
+ *                                                                      
+ * Contributors:                                                        
+ *    Isuru Udana - UI Integration in the Workbench
+ *******************************************************************************/
+
+package org.eclipse.ecf.salvo.ui.internal.views;
+
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.part.ViewPart;
+
+public class DigestView extends ViewPart {
+
+	public static final String ID = "org.eclipse.ecf.salvo.ui.internal.views.DigestView"; //$NON-NLS-1$
+	private final FormToolkit toolkit = new FormToolkit(Display.getCurrent());
+
+	public DigestView() {
+	}
+
+	/**
+	 * Create contents of the view part.
+	 * @param parent
+	 */
+	@Override
+	public void createPartControl(Composite parent) {
+		Composite container = toolkit.createComposite(parent, SWT.NONE);
+		toolkit.paintBordersFor(container);
+
+		createActions();
+		initializeToolBar();
+		initializeMenu();
+	}
+
+	public void dispose() {
+		toolkit.dispose();
+		super.dispose();
+	}
+
+	/**
+	 * Create the actions.
+	 */
+	private void createActions() {
+		// Create the actions
+	}
+
+	/**
+	 * Initialize the toolbar.
+	 */
+	private void initializeToolBar() {
+		IToolBarManager tbm = getViewSite().getActionBars().getToolBarManager();
+	}
+
+	/**
+	 * Initialize the menu.
+	 */
+	private void initializeMenu() {
+		IMenuManager manager = getViewSite().getActionBars().getMenuManager();
+	}
+
+	@Override
+	public void setFocus() {
+		// Set the focus
+	}
+
+}
