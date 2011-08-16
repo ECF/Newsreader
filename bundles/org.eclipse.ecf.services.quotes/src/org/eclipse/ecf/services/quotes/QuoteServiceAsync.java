1b55f1d78c55f90979072a29e631effe70682250
Wim Jongman <wim.jongman@remainsoftware.com>
2011-08-16 23:06:08 +0200

    Temporary fix for Async quote service. Relies on ecf.remoteservices

--- bundles/org.eclipse.ecf.services.quotes/src/org/eclipse/ecf/services/quotes/QuoteServiceAsync.java
+++ bundles/org.eclipse.ecf.services.quotes/src/org/eclipse/ecf/services/quotes/QuoteServiceAsync.java
@@ -2,7 +2,7 @@ package org.eclipse.ecf.services.quotes;
 
 //import org.eclipse.ecf.remoteservice.IAsyncCallback;
 //import org.eclipse.ecf.remoteservice.IAsyncRemoteServiceProxy;
-import org.eclipse.equinox.concurrent.future.IFuture;
+//import org.eclipse.equinox.concurrent.future.IFuture;
 
 @SuppressWarnings("restriction")
 public interface QuoteServiceAsync /* extends IAsyncRemoteServiceProxy*/ {
@@ -12,7 +12,7 @@ public interface QuoteServiceAsync /* extends IAsyncRemoteServiceProxy*/ {
 	 * 
 	 * @see org.eclipse.ecf.services.quotes.QuoteService#getAllQuotes()
 	 */
-	public IFuture getAllQuotesAsync();
+//	public IFuture getAllQuotesAsync();
 	
 	/**
 	 * Callback invocation
