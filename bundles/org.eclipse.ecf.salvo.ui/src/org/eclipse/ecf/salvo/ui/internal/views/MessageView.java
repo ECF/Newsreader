bdfe747071eb24bab1e092f4b4c6c0982c976c16
Wim Jongman <wim.jongman@remainsoftware.com>
2013-03-21 14:17:35 +0100

    Convert the first view

--- bundles/org.eclipse.ecf.salvo.ui/src/org/eclipse/ecf/salvo/ui/internal/views/MessageView.java
+++ bundles/org.eclipse.ecf.salvo.ui/src/org/eclipse/ecf/salvo/ui/internal/views/MessageView.java
@@ -15,9 +15,18 @@ import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.apache.james.mime4j.codec.DecoderUtil;
 import org.apache.james.mime4j.parser.MimeStreamParser;
 import org.eclipse.core.runtime.SafeRunner;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 import org.eclipse.ecf.protocol.nntp.core.ServerStoreFactory;
 import org.eclipse.ecf.protocol.nntp.model.IArticle;
 import org.eclipse.ecf.protocol.nntp.model.SALVO;
@@ -29,7 +38,6 @@ import org.eclipse.jface.util.SafeRunnable;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
@@ -37,17 +45,16 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.ui.ISelectionListener;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.widgets.Label;
 
-public class MessageView extends ViewPart implements ISelectionListener,
-		ISelectionProvider {
+public class MessageView implements ISelectionProvider {
 
 	private IArticle article;
 
 	private Composite parent;
+
+	@Inject
+	MPart part;
 
 	public static String ID = "org.eclipse.ecf.salvo.ui.internal.views.messageView";
 
@@ -55,23 +62,29 @@ public class MessageView extends ViewPart implements ISelectionListener,
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public void createPartControl(Composite parent) {
+	@PostConstruct
+	public void createPartControl(Composite parent, ESelectionService service) {
 		this.parent = parent;
 		final GridLayout gridLayout_1 = new GridLayout();
 		gridLayout_1.marginWidth = 0;
 		gridLayout_1.marginHeight = 0;
 		parent.setLayout(gridLayout_1);
+		
+		description = new Label(parent, SWT.NONE);
+		description.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false,
+				false));
+		description.setText("Please select an article");
+		
 
 		initializeToolBar();
-		PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-				.getSelectionService().addPostSelectionListener(this);
-		getSite().setSelectionProvider(this);
+		// service.addPostSelectionListener(this);
+		// getSite().setSelectionProvider(this);
 
 	}
 
-	@Override
+	@Focus
 	public void setFocus() {
+		description.setFocus();
 
 	}
 
@@ -80,97 +93,11 @@ public class MessageView extends ViewPart implements ISelectionListener,
 		// getViewSite().getActionBars().getToolBarManager();
 	}
 
-	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-
-		IArticle newArticle = null;
-		ISalvoResource resource = null;
-
-		if (part == this)
-			return;
-
-		if (selection instanceof IStructuredSelection) {
-			IStructuredSelection struct = (IStructuredSelection) selection;
-
-			if (struct.size() != 1) {
-				return;
-			}
-
-			if (!(struct.getFirstElement() instanceof ISalvoResource)) {
-				return;
-			}
-
-			if (!(((ISalvoResource) struct.getFirstElement()).getObject() instanceof IArticle)) {
-				return;
-			}
-
-			resource = (ISalvoResource) struct.getFirstElement();
-			newArticle = (IArticle) resource.getObject();
-			setSelection(selection);
-
-			if (article != newArticle) {
-
-				// FIXME same code is used in ReplyView
-
-				try {
-					article = newArticle;
-
-					setContentDescription("From: " + article.getFrom()
-							+ "  To: " + article.getXRef());
-					setPartName(DecoderUtil.decodeEncodedWords(article
-							.getSubject()));
-
-					article.setRead(true);
-					article.setThreadAttributes(ServerStoreFactory.instance()
-							.getServerStoreFacade().getAllFollowUps(article));
-					ServerStoreFactory.instance().getServerStoreFacade()
-							.updateArticle(article);
-
-					StringBuffer buffer = new StringBuffer();
-					String[] body = (String[]) ServerStoreFactory.instance()
-							.getServerStoreFacade().getArticleBody(article);
-					for (String line : body) {
-						buffer.append(line + SALVO.CRLF);
-					}
-
-					MimeArticleContentHandler handler = new MimeArticleContentHandler(
-							article);
-					MimeStreamParser parser = new MimeStreamParser();
-					parser.setContentHandler(handler);
-
-					parser.parse(new ByteArrayInputStream(buffer.toString()
-							.getBytes()));
-
-					if (!parent.isDisposed())
-						for (Control child : parent.getChildren()) {
-							child.dispose();
-						}
-
-					ArticleWidgetBuilder.build(parent, article, handler);
-					// composite.setLayout(gridLayout);
-					parent.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
-							true));
-					parent.layout(true);
-
-					// FIXME tight coupling is a no go. Need to set the read
-					// state
-					// some other way
-					if (part instanceof ArticlePanel) {
-						((ArticlePanel) part).updateArticle(resource);
-					}
-
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
-			}
-
-			getViewSite().getPage().bringToTop(this);
-		}
-
-	}
-
 	List<ISelectionChangedListener> listeners = new ArrayList<ISelectionChangedListener>();
 
 	ISelection theSelection = StructuredSelection.EMPTY;
+
+	private Label description;
 
 	public void addSelectionChangedListener(ISelectionChangedListener listener) {
 		listeners.add(listener);
@@ -199,5 +126,78 @@ public class MessageView extends ViewPart implements ISelectionListener,
 				}
 			});
 		}
+	}
+
+	@Inject
+	public void selectionChanged(
+			@Optional @Named(IServiceConstants.ACTIVE_SELECTION) ISalvoResource resource) {
+
+		IArticle newArticle = null;
+
+		if (resource == null || !(resource.getObject() instanceof IArticle)) {
+			return;
+		}
+
+		newArticle = (IArticle) resource.getObject();
+		// setSelection(selection);
+
+		if (article != newArticle) {
+
+			// FIXME same code is used in ReplyView
+
+			try {
+				article = newArticle;
+
+				part.setDescription("From: " + article.getFrom() + "  To: "
+						+ article.getXRef());
+				part.setLabel(DecoderUtil.decodeEncodedWords(article
+						.getSubject()));
+
+				article.setRead(true);
+				article.setThreadAttributes(ServerStoreFactory.instance()
+						.getServerStoreFacade().getAllFollowUps(article));
+				ServerStoreFactory.instance().getServerStoreFacade()
+						.updateArticle(article);
+
+				StringBuffer buffer = new StringBuffer();
+				String[] body = (String[]) ServerStoreFactory.instance()
+						.getServerStoreFacade().getArticleBody(article);
+				for (String line : body) {
+					buffer.append(line + SALVO.CRLF);
+				}
+
+				MimeArticleContentHandler handler = new MimeArticleContentHandler(
+						article);
+				MimeStreamParser parser = new MimeStreamParser();
+				parser.setContentHandler(handler);
+
+				parser.parse(new ByteArrayInputStream(buffer.toString()
+						.getBytes()));
+
+				if (!parent.isDisposed())
+					for (Control child : parent.getChildren()) {
+						child.dispose();
+					}
+
+				ArticleWidgetBuilder.build(parent, article, handler);
+				// composite.setLayout(gridLayout);
+				parent.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
+						true));
+				parent.layout(true);
+
+				// FIXME tight coupling is a no go. Need to set the read
+				// state
+				// some other way
+				if (part instanceof ArticlePanel) {
+					((ArticlePanel) part).updateArticle(resource);
+				}
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		part.setOnTop(true);
+
 	}
 }
