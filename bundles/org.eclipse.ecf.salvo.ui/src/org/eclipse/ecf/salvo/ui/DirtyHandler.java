cdfbd6410d48550a5de140c2d2d41eab81b9f63f
Wim Jongman <wim.jongman@remainsoftware.com>
2013-03-26 17:11:34 +0100

    make the handler more complex

--- bundles/org.eclipse.ecf.salvo.ui/src/org/eclipse/ecf/salvo/ui/DirtyHandler.java
+++ bundles/org.eclipse.ecf.salvo.ui/src/org/eclipse/ecf/salvo/ui/DirtyHandler.java
@@ -5,20 +5,37 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ecf.protocol.nntp.model.IServer;
 import org.eclipse.ecf.salvo.ui.internal.resources.ISalvoResource;
-import org.eclipse.ecf.salvo.ui.tools.SelectionUtil;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class DirtyHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
-		ISalvoResource resource = (ISalvoResource) SelectionUtil
-				.getFirstObjectFromSelection(HandlerUtil
-						.getCurrentSelection(event), ISalvoResource.class);
-		if (resource != null && resource.getObject() instanceof IServer) {
-			((IServer) resource.getObject()).setDirty(true);
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		
+		if(selection == null){
+			return null;
 		}
+		if (!(selection instanceof IStructuredSelection)) {
+			return null;
+		}
+		IStructuredSelection struct = (IStructuredSelection) selection;
+		if (struct.isEmpty()) {
+			return null;
+		}
+		if (!(struct.getFirstElement() instanceof ISalvoResource)) {
+			return null;
+		}
+		
+		ISalvoResource resource = (ISalvoResource) struct.getFirstElement();
+		if (!(resource.getObject() instanceof IServer)) {
+			return null;
+		}
+
+		IServer server = (IServer) resource.getObject();
+		server.setDirty(true);
 		return null;
 	}
-
 }
